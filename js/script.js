5399577ccbfe26665d4c5a2654ed64b937566d9b
Sina <bekarsina@gmail.com>
2022-06-26 17:22:17 +0430

    projrct of keycode

---
+++ js/script.js
@@ -0,0 +1,23 @@
+﻿let $ = document
+let locationKey = $.querySelector('.location')
+let starter = $.querySelector('.starter')
+let ascii = $.querySelector('.ascii')
+let infos = $.querySelector('.infos')
+let key = $.querySelector('.key')
+let which = $.querySelector('.which')
+let code = $.querySelector('.code')
+let keyCodePress = $.querySelector('.keyCode')
+
+document.body.addEventListener('keydown', function (event) {
+	
+	starter.style.display = 'none'
+	ascii.style.display = 'flex'
+	infos.style.display = 'flex'
+
+
+	keyCodePress.innerHTML = event.keyCode
+	key.innerHTML = event.key
+	locationKey.innerHTML = event.location
+	which.innerHTML = event.which
+	code.innerHTML = event.code
+})
